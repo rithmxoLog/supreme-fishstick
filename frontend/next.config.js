298a75b422e4d4a4c6d5321f4ab7698d564b581b
Logan Carpenter <logan.carpenter@rithmxo.com>
2026-02-23 14:13:07 -0700

    appdeployer

--- frontend/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
-  reactStrictMode: true,
-  images: { unoptimized: true },
-};
-
-module.exports = nextConfig;
